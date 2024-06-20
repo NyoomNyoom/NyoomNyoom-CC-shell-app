03c2742f13841f04d2dacd72c8b76d17b5afd6ff
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-21 00:53:15 +1200

    Added an echo function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <complex>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -6,14 +7,16 @@ int main() {
   std::cerr << std::unitbuf;  
 
   while (true){
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
 
-    if (input == "exit 0"){
-      return 0;
-    } else {
-      std::cout << input <<": command not found\n";
+    if (input.substr(0,4) == "exit") { //Exit function
+        return 0;
+    } else if (input.substr(0,4) == "echo") { //Echo function
+        std::cout << input.substr(4)<<"\n";
+    } else { //Command not found function
+        std::cout << input <<": command not found\n";
     }
   }
 }
