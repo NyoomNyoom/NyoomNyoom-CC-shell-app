3b94a68a046ff0b111cbca566b8f78de192b19a7
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-21 01:15:03 +1200

    Made a working type command

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+    std::cout << std::unitbuf; // console out.
+    std::cerr << std::unitbuf; // console error.
     std::string commands[3] = { "exit", "echo", "type" };
 
     while (true) {
@@ -19,7 +19,20 @@ int main() {
             std::cout << input.substr(5) << "\n";
         }
         else if (input.substr(0, 4) == commands[2]) { //Type function
+            bool found = false; //variable to hold a boolean for when a function is found.
 
+            for (int i = 0; i < 4; i++) { // Loops over the array to find if the command exists in the array.
+                if (commands[i] == input.substr(5)) {
+                    found = true;
+                    break; //Stops the for loop from continuing.
+                }
+            }
+
+            if (found) {
+                std::cout << input.substr(5) << " is a builtin\n";
+            } else {
+                std::cout << input.substr(5) << " not found\n";
+            }
         }
         else { //Command not found function
             std::cout << input << ": command not found\n";
