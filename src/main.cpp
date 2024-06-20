a2182a50860841d0923c5c8fb1518924387880d9
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-21 01:04:56 +1200

    Cleaned the code up

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 #include <complex>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;  
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    std::string commands[3] = { "exit", "echo", "type" };
 
-  while (true){
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
+    while (true) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
 
-    if (input.substr(0,4) == "exit") { //Exit function
-        return 0;
-    } else if (input.substr(0,4) == "echo") { //Echo function
-        std::cout << input.substr(5)<<"\n";
-    } else { //Command not found function
-        std::cout << input <<": command not found\n";
+        if (input.substr(0, 4) == commands[0]) { //Exit function
+            return 0;
+        }
+        else if (input.substr(0, 4) == commands[1]) { //Echo function
+            std::cout << input.substr(5) << "\n";
+        }
+        else if (input.substr(0, 4) == commands[2]) { //Type function
+
+        }
+        else { //Command not found function
+            std::cout << input << ": command not found\n";
+        }
     }
-  }
 }
